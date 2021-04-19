fa177a3df25fb7167dfe4a999f704932b118893e
Darrick Truong <truong.darrick@gmail.com>
2021-04-19 13:18:17 -0700

    Add MyModel to the project

---
+++ LearningGitHub/MyModel.swift
@@ -0,0 +1,21 @@
+//
+//  MyModel.swift
+//  LearningGitHub
+//
+//  Created by Darrick on 4/19/21.
+//  Copyright © 2021 sTruong Code. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyModel_Previews: PreviewProvider {
+    static var previews: some View {
+        MyModel()
+    }
+}
