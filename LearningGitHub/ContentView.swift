31e07787d5eadb682514c533d224e1eb9aabd253
Darrick Truong <truong.darrick@gmail.com>
2021-04-19 12:45:12 -0700

    Initial Commit

---
+++ LearningGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGitHub
+//
+//  Created by Darrick on 4/19/21.
+//  Copyright © 2021 sTruong Code. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
